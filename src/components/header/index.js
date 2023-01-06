104a0f2602412fefa466b880713916bb2f9c16ed
Martvie <marviedias@gmail.com>
2023-01-05 22:51:01 -0300

    background organization and input creation

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,7 +3,7 @@ import './styles.css';
 const Header = () => {
     return(
     <header>
-        <h1> GitFind</h1>
+        <h1> GitFind </h1>
     </header>
     )
 }
--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -1,9 +1,21 @@
 import { Header } from "../../components/header";
 
+import background from "../../components/assets/background.png";
+
+import "./styles.css";
+
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <div className="content">
+        <img src={background} alt="logo github para o background" className="background" />
+        <div className="info">
+<input name="user" placeholder="@martvie"/>
+<button>Buscar</button>
+
+        </div>
+      </div>
     </div>
   );
 }
