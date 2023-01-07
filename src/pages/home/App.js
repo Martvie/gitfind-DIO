9853edfa41e2061e663d9c3a7c156361d9205ff6
Martvie <marviedias@gmail.com>
2023-01-06 23:47:54 -0300

    profile area creation

--- src/pages/home/App.js
+++ src/pages/home/App.js
@@ -10,11 +10,26 @@ function App() {
       <Header />
       <div className="content">
         <img src={background} alt="logo github para o background" className="background" />
-        <div className="info">
-<input name="user" placeholder="@martvie"/>
-<button>Buscar</button>
 
+        <div className="info">
+          <div>
+            <input name="user" placeholder="@martvie" />
+            <button>Buscar</button>
+          </div>
+
+          <div className="profile">
+            <img src="https://avatars.githubusercontent.com/u/79718740?s=400&u=a81131afdb7fd141923534501830af3a059cc4cf&v=4" className="profile-picture" alt="Foto de perfil do usuário" />
+
+            <div>
+              <h3>Nome</h3>
+              <span>@fukuz</span>
+              <p>Descrição</p>
+            </div>
+
+          </div>
+          <hr/>
         </div>
+
       </div>
     </div>
   );
